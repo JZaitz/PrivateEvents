e2b7a0cc00934b9fab9b4844c719645a3cb05867
EC2 Default User <ec2-user@ip-172-31-41-159.us-east-2.compute.internal>
2018-06-22 19:55:30 +0000

    first commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,34 @@
+class EventsController < ApplicationController
+    
+    
+  def new
+    @event = user.events.build
+  end
+    
+    
+  def create
+    @event = user.events.build(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    # @events = Event.paginate(page: params[:page]).per_page(10)
+    @events_upcoming = Event.upcoming.paginate(page: params[:upcoming])
+    @events_past = Event.past.paginate(page: params[:past])
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :location, :description, :date)
+    end
+
+end
