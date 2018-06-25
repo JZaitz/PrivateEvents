4daf74ed566d79239b78fd16f605b965ff200740
EC2 Default User <ec2-user@ip-172-31-41-159.us-east-2.compute.internal>
2018-06-25 05:25:25 +0000

    event controller update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,34 +1,40 @@
 class EventsController < ApplicationController
-    
+  
+  def index
+    @events = Event.all
+  end
     
   def new
-    @event = @user.events.build
+    #@event = @user.events.build
   end
     
     
   def create
-    @event = @user.events.build(event_params)
-    if @event.save
-      redirect_to event_path(@event)
-    else
-      render 'new'
-    end
+    #@event = Event.new(event_params)
+    #@event.user_id = params[:user_id]
 
+    #@event.save
+
+    #redirect_to user_path(@event.user)
+
+  end
+  
+  def edit
   end
 
   def show
-    @event = Event.find(params[:id])
+    #@event = Event.find(params[:id])
   end
 
   def index
     # @events = Event.paginate(page: params[:page]).per_page(10)
-    @events_upcoming = Event.upcoming.paginate(page: params[:upcoming])
-    @events_past = Event.past.paginate(page: params[:past])
+    #@events_upcoming = Event.upcoming.paginate(page: params[:upcoming])
+    #@events_past = Event.past.paginate(page: params[:past])
   end
 
   private
     def event_params
-      params.require(:event).permit(:title, :location, :description, :date)
+      #params.require(:event).permit(:title, :location, :description, :date, :user)
     end
 
 end
